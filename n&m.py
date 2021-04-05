1ebdcc936aaf6f2afb2d4d27d916e9f0c3bb21be
TAHURASFRY <80903527+TAHURASFRY@users.noreply.github.com>
2021-04-05 22:56:01 +1200

    Update n&m.py

--- n&m.py
+++ n&m.py
@@ -1,3 +1,4 @@
+import termcolor2
 m = int(input())
 n = int(input())
 
@@ -5,14 +6,15 @@ for _ in range(0 , n):
     if _ % 2 == 0:
         for _ in range(0 , m):
             if _ % 2 == 0:
-                 print("$" , end=' ')
+                 print(termcolor2.colored("$", color="yellow") , end=' ')
             elif _ % 2 == 1:
-                print("@" , end =' ')
+                print(termcolor2.colored("@", color="red") , end =' ')
         print()
     if _ % 2 == 1:
         for _ in range(0 , m):
             if _ % 2 == 1:
-                print("$" , end=' ')
+                print(termcolor2.colored("$", color="yellow") , end=' ')
             elif _ % 2 == 0:
-                print("@" , end =' ')
+                print(termcolor2.colored("@", color="red") , end =' ')
         print()
+
