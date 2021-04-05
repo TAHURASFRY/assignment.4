9ab829998d7c836a26f07ee1a5b78e69593f4c36
TAHURASFRY <80903527+TAHURASFRY@users.noreply.github.com>
2021-04-05 23:14:53 +1200

    Update n&m.py

--- n&m.py
+++ n&m.py
@@ -6,15 +6,15 @@ for _ in range(0 , n):
     if _ % 2 == 0:
         for _ in range(0 , m):
             if _ % 2 == 0:
-                 print(termcolor2.colored("$", color="yellow") , end=' ')
+                 print(termcolor2.colored("#", color="yellow") , end=' ')
             elif _ % 2 == 1:
-                print(termcolor2.colored("@", color="red") , end =' ')
+                print(termcolor2.colored("*", color="red") , end =' ')
         print()
     if _ % 2 == 1:
         for _ in range(0 , m):
             if _ % 2 == 1:
-                print(termcolor2.colored("$", color="yellow") , end=' ')
+                print(termcolor2.colored("#", color="yellow") , end=' ')
             elif _ % 2 == 0:
-                print(termcolor2.colored("@", color="red") , end =' ')
+                print(termcolor2.colored("*", color="red") , end =' ')
         print()
 
