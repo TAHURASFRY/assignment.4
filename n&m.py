9e13baed5ad49a24c9a0b1f5d845af61b661114c
TAHURASFRY <80903527+TAHURASFRY@users.noreply.github.com>
2021-04-05 22:47:03 +1200

    Update n&m.py

--- n&m.py
+++ n&m.py
@@ -1,18 +1,18 @@
 m = int(input())
 n = int(input())
 
-for j in range(0 , n):
-    if j % 2 == 0:
-        for i in range(0 , m):
-            if i % 2 == 0:
+for _ in range(0 , n):
+    if _ % 2 == 0:
+        for _ in range(0 , m):
+            if _ % 2 == 0:
                  print("$" , end=' ')
-            elif i % 2 == 1:
+            elif _ % 2 == 1:
                 print("@" , end =' ')
         print()
-    if j % 2 == 1:
-        for i in range(0 , m):
-            if i % 2 == 1:
+    if _ % 2 == 1:
+        for _ in range(0 , m):
+            if _ % 2 == 1:
                 print("$" , end=' ')
-            elif i % 2 == 0:
+            elif _ % 2 == 0:
                 print("@" , end =' ')
         print()
